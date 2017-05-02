acd9ee3c7ea90336713ecac50019cf0450137cef
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-02 13:36:17 -0500

    products and i_sessions migrations  written

--- app/db/migrations/20170502131740_inventory_schema.js
+++ app/db/migrations/20170502131740_inventory_schema.js
@@ -1,8 +1,32 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('products', (table) => {
+    table.increments();
+    table.bigInteger('upc_code').unsigned().notNullable().unique();
+    table.string('name').notNullable();
+    table.string('image');
+    table.string('description', 1000).notNullable();
+    table.integer('popularity');
+  })
+  .createTable('inventory_sessions', (table) => {
+    table.increments();
+    table.timestamp('date').notNullable().defaultTo(knex.fn.now())
+    table.string('username')
+  })
+  .createTable('directors', (table) => {
+    table.increments()
+    table.string('name').notNullable().unique()
+    table.string('gender').notNullable()
+    table.integer('birthYear')
+    table.string('twitterHandle')
+  })
+  .createTable('shows_directors', (table) => {
+    table.increments()
+    table.integer('director_id').unsigned().references('directors.id')
+    table.integer('show_id').unsigned().references('shows.id')
+  })
 };
 
-exports.down = function(knex, Promise) {
+exports.down = (knex, Promise) => {
   
 };
