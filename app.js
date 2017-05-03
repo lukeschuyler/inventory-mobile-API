83a7f21e735d00dd6291dfe9d748dcbfe90eabea
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-03 14:04:34 -0500

    removed cors

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const { json, urlencoded } = require('body-parser');
 const routes = require('./app/routes/');
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express();
 
-app.use(cors())
+// app.use(cors())
 app.use(json())
-// app.use(urlencoded({ extended: false }));
+app.use(urlencoded({ extended: false }));
 
 app.use('/api/v1/', routes);
 
