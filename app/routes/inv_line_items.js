506ebe69fb38f5d56208175b61d80d96fac6d0ed
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-06 09:16:40 -0500

    added delete route for all routes

--- app/routes/inv_line_items.js
+++ app/routes/inv_line_items.js
@@ -2,12 +2,13 @@
 
 const { Router } = require('express');
 
-const { getAll, getOne, addItem } = require('../controllers/inv_line_itemCtrl');
+const { getAll, getOne, addItem, deleteItem } = require('../controllers/inv_line_itemCtrl');
 
 const router = Router();
 
 router.get('/inv_line_items', getAll);
 router.get('/inv_line_items/:id', getOne);
 router.post('/inv_line_items', addItem)
+router.delete('/inv_line_items/:id', deleteItem)
 
 module.exports = router;
--- app/routes/products.js
+++ app/routes/products.js
@@ -2,12 +2,13 @@
 
 const { Router } = require('express');
 
-const { getAll, getOne, addProduct } = require('../controllers/productCtrl');
+const { getAll, getOne, addProduct, deleteProduct } = require('../controllers/productCtrl');
 
 const router = Router();
 
 router.get('/products', getAll);
 router.get('/products/:id', getOne);
 router.post('/products', addProduct)
+router.delete('/products/:id', deleteProduct)
 
 module.exports = router;
