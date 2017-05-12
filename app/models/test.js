280d7d7f69479a02df0f5da433265df4b026ff36
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-12 08:54:55 -0500

    queries for sessions in testing done

--- app/models/test.js
+++ app/models/test.js
@@ -1,10 +1,10 @@
-const RecLineItem = require('./rec_line_item.js')
+const RecSession = require('./rec_session.js')
 
 const data = {   product_id: 2,
           receiving_session_id: 3,
           quantity: 20 }
 
-RecLineItem.addItem(data)
+RecSession.addSession({username: 'lukeschuyler'})
 .then(res => {
   console.log(res.toJSON())
 })
