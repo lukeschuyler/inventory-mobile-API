b0b492e3f168a761e63543094876b1ee6ed7533d
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-15 18:19:08 -0500

    parsed amazon data in controller

--- app/controllers/amazonCtrl.js
+++ app/controllers/amazonCtrl.js
@@ -13,40 +13,34 @@ module.exports.search = (req, res, next) => {
   })
   .then(results => {
     let searchResults = []
-
-    for (let i = 0; i < results.length; i++) {
+    results.forEach(result => {
       let searchProduct = {}
-      searchProduct.name = results.data[i].ItemAttributes[0].Title[0];
+      searchProduct.name = result.ItemAttributes[0].Title[0];
 
-      if (results.data[i].ItemAttributes[0].Brand === undefined) {
-          continue;
-        } else {
-          searchProduct.company = results.data[i].ItemAttributes[0].Brand[0];
-        }
-        
-      searchProduct.link = results.data[i].DetailPageURL[0];
-      searchProduct.ASIN = results.data[i].ASIN[0];
-      searchProduct.UPC = results.data[i].ItemAttributes[0].UPC[0]
+      searchProduct.link = result.DetailPageURL[0];
+      searchProduct.ASIN = result.ASIN[0];
+      searchProduct.UPC = result.ItemAttributes[0].UPC[0]
 
-      if(results.data[i].ImageSets) {
-        searchProduct.image = results.data[i].ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
+      if(result.ImageSets) {
+        searchProduct.image = result.ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
       } else {
         searchProduct.image = 'No Image Available'
       }
 
-      if(results.data[i].ItemAttributes[0].Feature) {
-        searchProduct.description = p.ItemAttributes[0].Feature[0];
+      if(result.ItemAttributes[0].Feature) {
+        searchProduct.description = result.ItemAttributes[0].Feature[0];
       } else {
         searchProduct.description = 'No Description Available'
       }
 
-      if(results.data[i].ItemAttributes[0].ListPrice) {
-        searchProduct.price = (results.data[i].ItemAttributes[0].ListPrice[0].Amount[0] / 100).toFixed(2);
+      if(result.ItemAttributes[0].ListPrice) {
+        searchProduct.price = (result.ItemAttributes[0].ListPrice[0].Amount[0] / 100).toFixed(2);
       } else {
         searchProduct.price = 'No Price Available'
       }
       searchResults.push(searchProduct)
-    }
+      console.log(searchResults)
+    })
      res.status(200).json(searchResults)
   })
   .catch(err => next(err));
